84a7e073af57f786d4e455948d5ac5c1da63dea1
Moon Dev <115879186+moondevonyt@users.noreply.github.com>
2025-02-05 08:35:22 -0400

    clips agent that can make money

---
+++ src/agents/clips_agent.py
@@ -0,0 +1,53 @@
+'''
+this ai agent will be able to take long videos and make short videos
+
+enabling this ai agent to help you earn money while you learn to code
+
+you get paid by most streamers to make clips of their streams
+
+i have so much gold in my videos and things that can truly help people
+
+if you want to learn how to code and get paid while you do it
+
+you should just watch my videos and when you see something that you think is gold, you can make a clip out of it
+
+and you can get paid by me for that
+
+check out the #clips channel in discord for more info: https://discord.gg/XAw8US9aHT
+
+examples of people crushing it with my videos:
+
+https://www.youtube.com/@Algonomicstrades
+https://www.youtube.com/@moondevonytsnips
+
+they essentially get paid to watch my coding videos and make clips of the good parts
+
+because i dont have time to make clips i can only stream as im building my algos
+
+steps to your success as a moon dev clipper:
+1. find good long videos on my channel: https://www.youtube.com/@moondevonyt/videos 
+2. watch the video and find the parts that are good
+3. make a clip out of it (5 mins-2hours in length)
+4. upload the clip to your youtube channel (youtube only, no youtube shorts)
+5. when you hit 10,000 views, get paid $69
+
+Standard payout is $69 per 10,000 views but it increases based on your views per month..
+
+10,000 views per month = $69 per 10,000 views
+30,000 views per month = $89 per 10,000 views 
+50,000 views per month = $100 per 10,000 views
+100,000 views per month = $149 per 10,000 views
+
+BONUS GROUP: once you hit 10,000 views per month, you get access to a private channel in discord with training on how to get more views
+
+payments are via crypto on the 1st and 15th of every month, when you hit 10,000 views per month + just email the link, and your tracking of the views to moondevonyt@gmail.com
+
+* if in the USA, you can only earn up to $500 per year but in the rest of the world, you can earn unlimited.
+
+Where do you get the videos from?
+1. this dropbox has a bunch of my videos that i will upload to daily: https://www.dropbox.com/scl/fo/d0rjdyus9q3pok5nbmo7b/AM9LOmUDv8KIjmH6ypTALx0?rlkey=klg4tinvneqyui46r6851liwa&st=0zxfym3w&dl=0
+2. you have permission to download any of my youtube videos: https://www.youtube.com/@moondevonyt/videos
+
+
+
+'''
